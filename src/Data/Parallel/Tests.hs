43f39535d91bf28656b9bf424624ad3a6516cabf
Mauro Blanco <blancomau@gmail.com>
2015-09-05 17:28:29 -0300

    más tests

--- src/Data/Parallel/Tests.hs
+++ src/Data/Parallel/Tests.hs
@@ -55,7 +55,7 @@ case3 = do
     assertEquals expected result
     
 case4 = do
-    putStrLn "unfold -> until -> fold, stream infinito"
+    putStrLn "unfold inf -> until -> fold"
     let input = [1, 2 ..]
     let expected = takeUntil (+) 0 (>10) input
     let (f, z) = toUnfold input
@@ -89,8 +89,124 @@ case6 = do
     result <- sReduce (:) [] s3
     assertEquals expected result
 
+case7 = do
+    putStrLn "unfold inf -> map -> until -> fold"
+    let input = [1, 2 ..]
+    let expected = takeUntil (+) 0 (>10) . map (+1) $ input
+    let (f, z) = toUnfold input
+    s1 <- sUnfold f z
+    s2 <- sMap (+1) s1
+    s3 <- sUntil (+) 0 (>10) s2
+    result <- sReduce (:) [] s3
+    assertEquals expected result
 
+case8 = do
+    putStrLn "unfold inf -> filter -> until -> fold"
+    let input = [1, 2 ..]
+    let expected = takeUntil (+) 0 (>10) . filter odd $ input
+    let (f, z) = toUnfold input
+    s1 <- sUnfold f z
+    s2 <- sFilter odd s1
+    s3 <- sUntil (+) 0 (>10) s2
+    result <- sReduce (:) [] s3
+    assertEquals expected result
 
-tests = [case1, case2, case3, case4, case5, case6]
+case9 = do
+    putStrLn "unfold inf ->"
+    putStrLn "              join -> fold"
+    putStrLn "unfold inf ->"
+    let input1 = [1, 3 ..]
+    let input2 = [2, 4 ..]
+    let expected = takeUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 10) $ zip input1 input2
+    let (f1, z1) = toUnfold input1
+    let (f2, z2) = toUnfold input2
+    s1 <- sUnfold f1 z1
+    s2 <- sUnfold f2 z2
+    s3 <- sJoin s1 s2
+    s4 <- sUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 100) s3
+    result <- sReduce (:) [] s4
+    assertEquals expected result
+
+case10 = do
+    putStrLn "unfold     ->"
+    putStrLn "              join -> fold"
+    putStrLn "unfold inf ->"
+    let input1 = [1, 3, 5, 7, 9, 11]
+    let input2 = [2, 4 ..]
+    let expected = takeUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 10) $ zip input1 input2
+    let (f1, z1) = toUnfold input1
+    let (f2, z2) = toUnfold input2
+    s1 <- sUnfold f1 z1
+    s2 <- sUnfold f2 z2
+    s3 <- sJoin s1 s2
+    s4 <- sUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 100) s3
+    result <- sReduce (:) [] s4
+    assertEquals expected result
+    
+case11 = do
+    putStrLn "unfold inf ->"
+    putStrLn "              join -> fold"
+    putStrLn "unfold     ->"
+    let input1 = [1, 3 ..]
+    let input2 = [2, 4, 6, 8, 10, 12]
+    let expected = takeUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 10) $ zip input1 input2
+    let (f1, z1) = toUnfold input1
+    let (f2, z2) = toUnfold input2
+    s1 <- sUnfold f1 z1
+    s2 <- sUnfold f2 z2
+    s3 <- sJoin s1 s2
+    s4 <- sUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 100) s3
+    result <- sReduce (:) [] s4
+    assertEquals expected result
+
+case12 = do
+    putStrLn "unfold inf ->"
+    putStrLn "              join -> fold"
+    putStrLn "unfold inf ->"
+    let input1 = [1, 3 ..]
+    let input2 = [2, 4 ..]
+    let expected = takeUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 1000) $ zip input1 input2
+    let (f1, z1) = toUnfold input1
+    let (f2, z2) = toUnfold input2
+    s1 <- sUnfold f1 z1
+    s2 <- sUnfold f2 z2
+    s3 <- sJoin s1 s2
+    s4 <- sUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 1000) s3
+    result <- sReduce (:) [] s4
+    assertEquals expected result
+
+case13 = do
+    putStrLn "unfold     ->"
+    putStrLn "              join -> fold"
+    putStrLn "unfold inf ->"
+    let input1 = [1, 3, 5, 7, 9, 11]
+    let input2 = [2, 4 ..]
+    let expected = takeUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 1000) $ zip input1 input2
+    let (f1, z1) = toUnfold input1
+    let (f2, z2) = toUnfold input2
+    s1 <- sUnfold f1 z1
+    s2 <- sUnfold f2 z2
+    s3 <- sJoin s1 s2
+    s4 <- sUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 1000) s3
+    result <- sReduce (:) [] s4
+    assertEquals expected result
+    
+case14 = do
+    putStrLn "unfold inf ->"
+    putStrLn "              join -> fold"
+    putStrLn "unfold     ->"
+    let input1 = [1, 3 ..]
+    let input2 = [2, 4, 6, 8, 10, 12]
+    let expected = takeUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 1000) $ zip input1 input2
+    let (f1, z1) = toUnfold input1
+    let (f2, z2) = toUnfold input2
+    s1 <- sUnfold f1 z1
+    s2 <- sUnfold f2 z2
+    s3 <- sJoin s1 s2
+    s4 <- sUntil (\z (a1, a2) -> z + a1 + a2) 0 (> 1000) s3
+    result <- sReduce (:) [] s4
+    assertEquals expected result
+
+tests = [case1, case2, case3, case4, case5, case6, case7, case8, case9, case10, case11, case12, case13, case14]
     
 testAll = sequence tests
